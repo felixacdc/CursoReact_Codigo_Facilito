a2292dbd92ca6603ac0b52948a1ebacb362fd69c
felixacdc <pfmata01@gmail.com>
2017-03-08 11:50:22 -0600

    [modify] 5.- Curso React - Components

--- app/dist/index.js
+++ app/dist/index.js
@@ -1,2 +1,18 @@
-require('./modulo1.js')
-require('./modulo2.js')
+import React from "react"
+import ReactDOM from "react-dom"
+
+class Layout extends React.Component {
+	constructor() {
+		super();
+		this.nombre = "Félix Méndez";
+	}
+	render() {
+		return (
+			<h1>Hola {this.nombre}</h1>
+		);
+	}
+}
+
+const app = document.getElementById("app");
+
+ReactDOM.render(<Layout/>, app);
--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
           loader: 'babel-loader',
           query: {
               presets: ['react', 'es2015', 'stage-0'],
-              plugins: ['react-html-attr', 'transform-class-properties', 'transform-decorators-legacy']
+              plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy']
           }
         }
       ]
